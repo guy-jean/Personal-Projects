e3eefba7bcd33775c95a20d4d7cf671b30836067
Ryan Brown <ryabro26@gmail.com>
2020-02-22 15:41:46 -0500

    Finished ChessPiece abstract class

--- Java-Projects/Chess/src/game/ChessPiece.java
+++ Java-Projects/Chess/src/game/ChessPiece.java
@@ -10,6 +10,13 @@ public abstract class ChessPiece {
 	private ChessPieceColor color;
 	private ChessBoard chessBoard;
 	
+	public ChessPiece(int file, int rank, ChessPieceColor color, ChessBoard chessBoard) {
+		this.file = file;
+		this.rank = rank;
+		this.color = color;
+		this.chessBoard = chessBoard;
+	}
+	
 	public boolean move(int targetFile, int targetRank) {
 		if (validMove(targetFile, targetRank)) {
 			capture(targetFile, targetRank);
@@ -21,7 +28,7 @@ public abstract class ChessPiece {
 	
 	public ChessPiece capture(int targetFile, int targetRank) {
 		Square currSquare = this.chessBoard.getSquare(this.file, this.rank);
-		Square targetSquare = this.chessBoard.getSquare(targetFile, targetFile);
+		Square targetSquare = this.chessBoard.getSquare(targetFile, targetRank);
 		ChessPiece targetPiece = targetSquare.getPiece();
 		
 		targetSquare.setPiece(this);
@@ -33,6 +40,28 @@ public abstract class ChessPiece {
 		return targetPiece;
 	}
 	
-	public abstract boolean validMove(int targetFile, int targetRank);
+	public  boolean validMove(int targetFile, int targetRank) {
+		return inBounds(targetFile, targetRank) && inRange(targetFile, targetRank) 
+				&& !occupiedBySameColor(targetFile, targetRank);
+	}
+
+	protected abstract boolean inRange(int targetFile, int targetRank);
+
+	private boolean inBounds(int targetFile, int targetRank) {
+		return targetFile >= 0 && targetFile < this.chessBoard.MAXFILE && 
+				targetRank >= 0 && targetRank < this.chessBoard.MAXRANK;
+	}
+	
+
+	private boolean occupiedBySameColor(int targetFile, int targetRank) {
+		Square targetSquare = this.chessBoard.getSquare(targetFile, targetRank);
+		ChessPiece targetPiece = targetSquare.getPiece();
+		
+		return targetPiece.getChessPieceColor() != this.getChessPieceColor();
+	}
+
+	public ChessPieceColor getChessPieceColor() {
+		return this.color;
+	}
 	
 }
---
+++ Java-Projects/Chess/src/game/Pawn.java
@@ -0,0 +1,11 @@
+package game;
+
+public class Pawn extends ChessPiece {
+
+	@Override
+	public boolean validMove(int targetFile, int targetRank) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
